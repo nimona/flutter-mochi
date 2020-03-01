1837a0d57a65e6190c5a63eb8204050d5d2b2ad2
Shiv <shivgadhia@gmail.com>
2020-03-01 07:27:32 +0000

    uses StreamBuilder to control ui states of messages stream

---
+++ lib/model/messageitem.dart
@@ -0,0 +1,13 @@
+class MessageItem {
+  MessageItem({
+    this.id,
+    this.conversationId,
+    this.user,
+    this.content,
+  });
+
+  final String id;
+  final String conversationId;
+  final String user;
+  final String content;
+}
