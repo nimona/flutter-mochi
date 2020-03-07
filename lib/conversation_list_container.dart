b493d38d11f0945e13c2f5011804ac292a4e14f8
Shiv <shivgadhia@gmail.com>
2020-03-07 14:52:39 +0000

    Adds dialog to allow user to create a conversation

--- lib/conversation_list_container.dart
+++ lib/conversation_list_container.dart
@@ -113,7 +113,7 @@ class _ConversationListContainer extends State<ConversationListContainer> {
             Scaffold(
               floatingActionButton: FloatingActionButton(
                 onPressed: () {
-                  Repository.get().startConversation("", "");
+                  _showCreateConversationDialog();
                 },
                 child: Icon(Icons.add),
                 backgroundColor: Colors.blue,
@@ -148,5 +148,63 @@ class _ConversationListContainer extends State<ConversationListContainer> {
         ),
       ),
     );
+  }
+
+  void _showCreateConversationDialog() {
+    final nameController = TextEditingController();
+    final topicController = TextEditingController();
+
+    var width = MediaQuery.of(context).size.width;
+    var dialogContentWidth = width * 0.8;
+
+    showDialog<bool>(
+        context: context,
+        builder: (BuildContext context) {
+          return new AlertDialog(
+            title: Text('Create a new conversation'),
+            contentPadding: const EdgeInsets.all(24.0),
+            content: new Container(
+                width: dialogContentWidth,
+                child: new Expanded(
+                  child: new Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      new TextField(
+                        controller: nameController,
+                        autofocus: true,
+                        decoration: new InputDecoration(
+                            labelText: 'Conversation name',
+                            hintText: 'eg. Worse than random'),
+                      ),
+                      new TextField(
+                        controller: topicController,
+                        autofocus: true,
+                        decoration: new InputDecoration(
+                            labelText: 'Topic', hintText: 'eg. typewriters'),
+                      )
+                    ],
+                  ),
+                )),
+            actions: <Widget>[
+              new FlatButton(
+                  child: const Text('CANCEL'),
+                  onPressed: () {
+                    Navigator.pop(context, false);
+                  }),
+              new FlatButton(
+                  child: const Text('CREATE'),
+                  onPressed: () {
+                    Navigator.pop(context, true);
+                  })
+            ],
+          );
+        }).then<void>((bool userClickedCreate) {
+      if (userClickedCreate == true &&
+          nameController.text.isNotEmpty &&
+          topicController.text.isNotEmpty) {
+        Repository.get()
+            .createConversation(nameController.text, topicController.text);
+      }
+    });
   }
 }
