5dcff386a9eda8e817da53f07fd9e9b822e54984
George Antoniadis <george@noodles.gr>
2020-03-24 11:02:50 +0000

    fix: merge conversation details

--- lib/conversation_details_container.dart
+++ lib/conversation_details_container.dart
@@ -2,69 +2,105 @@ import 'package:flutter/material.dart';
 import 'package:mochi/model/conversation.dart';
 import 'package:mochi/view/participant_name.dart';
 
-class ConversationDetailsContainer extends StatefulWidget {
-  ConversationDetailsContainer({
-    this.conversationUpdatedCallback,
-    this.selectedConversation,
-  });
+class ConversationDetailsContainer extends StatelessWidget {
+  const ConversationDetailsContainer({
+    Key key,
+    @required this.conversation,
+  }) : super(key: key);
 
-  ValueChanged<Conversation> conversationUpdatedCallback;
-  Conversation selectedConversation;
-
-  @override
-  _ConversationDetailsContainer createState() => _ConversationDetailsContainer(
-      conversationUpdatedCallback, selectedConversation);
-}
-
-class _ConversationDetailsContainer
-    extends State<ConversationDetailsContainer> {
-  _ConversationDetailsContainer(
-      this.conversationUpdatedCallback, this.selectedConversation);
-
-  ValueChanged<Conversation> conversationUpdatedCallback;
-  Conversation selectedConversation;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
+  final Conversation conversation;
 
   @override
   Widget build(BuildContext context) {
     final TextTheme textTheme = Theme.of(context).textTheme;
 
+    final nameController = TextEditingController(
+      text: conversation.name,
+    );
+    final topicController = TextEditingController(
+      text: conversation.topic,
+    );
+
+    // participants list
+    Widget ps = ListView(
+      primary: false,
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      children: conversation.participants.map((participant) {
+        return ListTile(
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(2),
+            child: Image.network(
+              "http://localhost:10100/displayPictures/" + participant.key,
+              height: 40,
+            ),
+          ),
+          title: ParticipantName(
+            context: context,
+            participant: participant,
+            textTheme: textTheme,
+          ),
+          subtitle: Text(
+            participant.key,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+          // onTap: () => conversationUpdatedCallback(participant),
+          // selected: widget.conversation == participant,
+          dense: true,
+        );
+      }).toList(),
+    );
+
+    // conversation details
+    Widget d = Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        new TextField(
+          // // controller: nameController,
+          // autofocus: true,
+          decoration: new InputDecoration(
+              labelText: 'Conversation name',
+              hintText: 'eg. Worse than random'),
+        ),
+        new TextField(
+          // controller: topicController,
+          // autofocus: true,
+          decoration: new InputDecoration(
+            labelText: 'Topic',
+            hintText: 'eg. typewriters',
+          ),
+        )
+      ],
+    );
+
+    // scaffold
     return Scaffold(
-      body: Scrollbar(
-        child: ListView(
-          children: selectedConversation.participants.map((participant) {
-            return ListTile(
-              leading: ClipRRect(
-                borderRadius: BorderRadius.circular(2),
-                child: Image.network(
-                  "http://localhost:10100/displayPictures/" + participant.key,
-                  height: 40,
-                ),
-              ),
-              title: ParticipantName(
-                context: context,
-                participant: participant,
-                textTheme: textTheme,
-              ),
-              subtitle: Text(
-                participant.key,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-              ),
-              // onTap: () => conversationUpdatedCallback(participant),
-              // selected: widget.selectedConversation == participant,
-              dense: true,
-            );
-          }).toList(),
+      body: Container(
+        margin: EdgeInsets.all(50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Conversation details",
+              style: textTheme.headline4,
+              textAlign: TextAlign.left,
+            ),
+            Container(
+              child: d,
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Text(
+              "Conversation participants",
+              style: textTheme.headline4,
+              textAlign: TextAlign.left,
+            ),
+            Expanded(
+              child: ps,
+            )
+          ],
         ),
       ),
     );
