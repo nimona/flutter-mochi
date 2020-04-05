c9ad64175811f9f7d19e8801699726e2272171af
George Antoniadis <george@noodles.gr>
2020-04-06 00:04:21 +0100

    feat: add gomobile for ios

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/shivamgadhia/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/shivamgadhia/AndroidStudioProjects/flutter_app"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/geoah/src/github.com/flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/geoah/src/github.com/nimona/flutter-mochi"
+export "FLUTTER_TARGET=/Users/geoah/src/github.com/nimona/flutter-mochi/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/shivamgadhia/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/geoah/src/github.com/flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=0.0.5"
+export "FLUTTER_BUILD_NUMBER=0.0.5"
+export "TRACK_WIDGET_CREATION=true"
