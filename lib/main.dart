47cb46c4a2d894d64a2143ebf102e418c9ea6c25
George Antoniadis <george@noodles.gr>
2021-01-17 16:12:40 +0000

    chore: release v0.2.3, enable automatic updates

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mochi',
       theme: ThemeData(
         brightness: Brightness.light,
         primarySwatch: Colors.blue,
